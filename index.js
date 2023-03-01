b0aa54371ef86e5bcf0fd28a9713aa66faaee0fb
bernsrepublic <bernsrepublic@gmail.com>
2023-02-28 20:52:23 -0800

    addEventListener addition of callback

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-// const inventory = newInventory();
-// move(inventory).to(0, 0);
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
 const character = newImage("assets/green-character/static.gif");
-let direction = null;
-let x = 100;
-let y = 250;
-move(character).to(100, 250);
+
+function handleDirectionChange() {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+// move(newImage("assets/tree.png")).withArrowKeys(200, 450); **test to move the tree img**
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
+// move(character).to(100, 250);
 
 // function moveCharacter() {
 //   if (direction === "west") {
@@ -22,7 +45,7 @@ move(character).to(100, 250);
 //   }
 //   character.style.left = x + "px";
 //   character.style.bottom = y + "px";
-// }
+// } **this part was replaced by setInterval function**
 setInterval(function () {
   if (direction === "west") {
     x = x - 1;
